fb7ddb629a33ce6264b6dcbc6f207747e0ff51ec
prinew11 <140534584+prinew11@users.noreply.github.com>
2023-07-26 01:55:44 +0800

    Add files via upload

---
+++ Debug/sources.mk
@@ -0,0 +1,67 @@
+################################################################################
+# 自动生成的文件。不要编辑！
+################################################################################
+
+ELF_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+OBJS := 
+SECONDARY_FLASH := 
+SECONDARY_SIZE := 
+ASM_DEPS := 
+S_DEPS := 
+S_UPPER_DEPS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+applications \
+board/CubeMX_Config/Src \
+board \
+libraries/Board_Drivers/lcd \
+libraries/HAL_Drivers \
+libraries/STM32F4xx_HAL/CMSIS/Device/ST/STM32F4xx/Source/Templates/gcc \
+libraries/STM32F4xx_HAL/CMSIS/Device/ST/STM32F4xx/Source/Templates \
+libraries/STM32F4xx_HAL/STM32F4xx_HAL_Driver/Src \
+packages/aht10-latest \
+packages/ap3216c-latest \
+packages/cJSON-latest \
+packages/icm20608-latest \
+packages/onenet-latest/samples \
+packages/onenet-latest/src \
+packages/pahomqtt-latest/MQTTClient-RT \
+packages/pahomqtt-latest/MQTTPacket/src \
+packages/rw007-v2.0.1/example \
+packages/rw007-v2.0.1/src \
+rt-thread/components/dfs/filesystems/devfs \
+rt-thread/components/dfs/src \
+rt-thread/components/drivers/i2c \
+rt-thread/components/drivers/ipc \
+rt-thread/components/drivers/misc \
+rt-thread/components/drivers/sensors \
+rt-thread/components/drivers/serial \
+rt-thread/components/drivers/spi \
+rt-thread/components/drivers/wlan \
+rt-thread/components/finsh \
+rt-thread/components/libc/compilers/common \
+rt-thread/components/libc/compilers/newlib \
+rt-thread/components/libc/posix/io/poll \
+rt-thread/components/net/lwip/lwip-2.0.3/src/api \
+rt-thread/components/net/lwip/lwip-2.0.3/src/apps/ping \
+rt-thread/components/net/lwip/lwip-2.0.3/src/core \
+rt-thread/components/net/lwip/lwip-2.0.3/src/core/ipv4 \
+rt-thread/components/net/lwip/lwip-2.0.3/src/netif \
+rt-thread/components/net/lwip/port \
+rt-thread/components/net/netdev/src \
+rt-thread/components/net/sal/dfs_net \
+rt-thread/components/net/sal/impl \
+rt-thread/components/net/sal/socket \
+rt-thread/components/net/sal/src \
+rt-thread/libcpu/arm/common \
+rt-thread/libcpu/arm/cortex-m4 \
+rt-thread/src \
+
