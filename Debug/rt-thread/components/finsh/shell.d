54cf98107f7eba0e7bbb9dbd5e71cd25e1f0e80c
prinew11 <140534584+prinew11@users.noreply.github.com>
2023-07-26 01:48:13 +0800

    Add files via upload

---
+++ Debug/rt-thread/components/finsh/shell.d
@@ -0,0 +1,46 @@
+rt-thread/components/finsh/shell.o: ../rt-thread/components/finsh/shell.c \
+ D:\RT-ThreadStudio\workspace\07\rtconfig_preinc.h \
+ D:\RT-ThreadStudio\workspace\07\rt-thread\include/rthw.h \
+ D:\RT-ThreadStudio\workspace\07\rt-thread\include/rtthread.h \
+ D:\RT-ThreadStudio\workspace\07/rtconfig.h \
+ D:\RT-ThreadStudio\workspace\07\rt-thread\include/rtdebug.h \
+ D:\RT-ThreadStudio\workspace\07\rt-thread\include/rtdef.h \
+ D:\RT-ThreadStudio\workspace\07\rt-thread\include/rtservice.h \
+ D:\RT-ThreadStudio\workspace\07\rt-thread\include/rtm.h \
+ D:\RT-ThreadStudio\workspace\07\rt-thread\components\finsh/finsh.h \
+ ../rt-thread/components/finsh/shell.h \
+ ../rt-thread/components/finsh/finsh.h \
+ ../rt-thread/components/finsh/msh.h \
+ D:\RT-ThreadStudio\workspace\07\rt-thread\components\libc\compilers\common\include/unistd.h \
+ D:\RT-ThreadStudio\workspace\07\rt-thread\components\libc\compilers\common\include/sys/unistd.h \
+ D:\RT-ThreadStudio\workspace\07\rt-thread\components\libc\compilers\newlib/fcntl.h
+
+D:\RT-ThreadStudio\workspace\07\rtconfig_preinc.h:
+
+D:\RT-ThreadStudio\workspace\07\rt-thread\include/rthw.h:
+
+D:\RT-ThreadStudio\workspace\07\rt-thread\include/rtthread.h:
+
+D:\RT-ThreadStudio\workspace\07/rtconfig.h:
+
+D:\RT-ThreadStudio\workspace\07\rt-thread\include/rtdebug.h:
+
+D:\RT-ThreadStudio\workspace\07\rt-thread\include/rtdef.h:
+
+D:\RT-ThreadStudio\workspace\07\rt-thread\include/rtservice.h:
+
+D:\RT-ThreadStudio\workspace\07\rt-thread\include/rtm.h:
+
+D:\RT-ThreadStudio\workspace\07\rt-thread\components\finsh/finsh.h:
+
+../rt-thread/components/finsh/shell.h:
+
+../rt-thread/components/finsh/finsh.h:
+
+../rt-thread/components/finsh/msh.h:
+
+D:\RT-ThreadStudio\workspace\07\rt-thread\components\libc\compilers\common\include/unistd.h:
+
+D:\RT-ThreadStudio\workspace\07\rt-thread\components\libc\compilers\common\include/sys/unistd.h:
+
+D:\RT-ThreadStudio\workspace\07\rt-thread\components\libc\compilers\newlib/fcntl.h:
