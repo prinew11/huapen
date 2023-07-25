94e134f584be8a5c2c924ccd1db93f37152afb89
prinew11 <140534584+prinew11@users.noreply.github.com>
2023-07-26 01:50:05 +0800

    Add files via upload

---
+++ Debug/rt-thread/components/drivers/ipc/subdir.mk
@@ -0,0 +1,37 @@
+################################################################################
+# 自动生成的文件。不要编辑！
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../rt-thread/components/drivers/ipc/completion.c \
+../rt-thread/components/drivers/ipc/dataqueue.c \
+../rt-thread/components/drivers/ipc/pipe.c \
+../rt-thread/components/drivers/ipc/ringblk_buf.c \
+../rt-thread/components/drivers/ipc/ringbuffer.c \
+../rt-thread/components/drivers/ipc/waitqueue.c \
+../rt-thread/components/drivers/ipc/workqueue.c 
+
+OBJS += \
+./rt-thread/components/drivers/ipc/completion.o \
+./rt-thread/components/drivers/ipc/dataqueue.o \
+./rt-thread/components/drivers/ipc/pipe.o \
+./rt-thread/components/drivers/ipc/ringblk_buf.o \
+./rt-thread/components/drivers/ipc/ringbuffer.o \
+./rt-thread/components/drivers/ipc/waitqueue.o \
+./rt-thread/components/drivers/ipc/workqueue.o 
+
+C_DEPS += \
+./rt-thread/components/drivers/ipc/completion.d \
+./rt-thread/components/drivers/ipc/dataqueue.d \
+./rt-thread/components/drivers/ipc/pipe.d \
+./rt-thread/components/drivers/ipc/ringblk_buf.d \
+./rt-thread/components/drivers/ipc/ringbuffer.d \
+./rt-thread/components/drivers/ipc/waitqueue.d \
+./rt-thread/components/drivers/ipc/workqueue.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+rt-thread/components/drivers/ipc/%.o: ../rt-thread/components/drivers/ipc/%.c
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -O0 -ffunction-sections -fdata-sections -Wall  -g -gdwarf-2 -DDEBUG -I"D:\RT-ThreadStudio\workspace\07" -I"D:\RT-ThreadStudio\workspace\07\applications" -I"D:\RT-ThreadStudio\workspace\07\board\CubeMX_Config\Inc" -I"D:\RT-ThreadStudio\workspace\07\board" -I"D:\RT-ThreadStudio\workspace\07\libraries\Board_Drivers\lcd" -I"D:\RT-ThreadStudio\workspace\07\libraries\Board_Drivers" -I"D:\RT-ThreadStudio\workspace\07\libraries\HAL_Drivers\CMSIS\Include" -I"D:\RT-ThreadStudio\workspace\07\libraries\HAL_Drivers\config" -I"D:\RT-ThreadStudio\workspace\07\libraries\HAL_Drivers" -I"D:\RT-ThreadStudio\workspace\07\libraries\STM32F4xx_HAL\CMSIS\Device\ST\STM32F4xx\Include" -I"D:\RT-ThreadStudio\workspace\07\libraries\STM32F4xx_HAL\STM32F4xx_HAL_Driver\Inc" -I"D:\RT-ThreadStudio\workspace\07\packages\aht10-latest" -I"D:\RT-ThreadStudio\workspace\07\packages\ap3216c-latest" -I"D:\RT-ThreadStudio\workspace\07\packages\cJSON-latest" -I"D:\RT-ThreadStudio\workspace\07\packages\icm20608-latest" -I"D:\RT-ThreadStudio\workspace\07\packages\onenet-latest\inc" -I"D:\RT-ThreadStudio\workspace\07\packages\pahomqtt-latest\MQTTClient-RT" -I"D:\RT-ThreadStudio\workspace\07\packages\pahomqtt-latest\MQTTPacket\src" -I"D:\RT-ThreadStudio\workspace\07\packages\rw007-v2.0.1\inc" -I"D:\RT-ThreadStudio\workspace\07\packages\rw007-v2.0.1" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\dfs\filesystems\devfs" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\dfs\include" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\drivers\include" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\drivers\sensors" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\drivers\spi" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\drivers\wlan" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\finsh" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\libc\compilers\common\include" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\libc\compilers\newlib" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\libc\posix\io\poll" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\libc\posix\io\stdio" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\libc\posix\ipc" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\net\lwip\lwip-2.0.3\src\include\netif" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\net\lwip\lwip-2.0.3\src\include" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\net\lwip\port" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\net\netdev\include" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\net\sal\impl" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\net\sal\include\dfs_net" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\net\sal\include\socket\sys_socket" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\net\sal\include\socket" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\components\net\sal\include" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\include" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\libcpu\arm\common" -I"D:\RT-ThreadStudio\workspace\07\rt-thread\libcpu\arm\cortex-m4" -include"D:\RT-ThreadStudio\workspace\07\rtconfig_preinc.h" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+
---
+++ Debug/rt-thread/components/drivers/spi/spi_dev.d
@@ -0,0 +1,29 @@
+rt-thread/components/drivers/spi/spi_dev.o: \
+ ../rt-thread/components/drivers/spi/spi_dev.c \
+ D:\RT-ThreadStudio\workspace\07\rtconfig_preinc.h \
+ D:\RT-ThreadStudio\workspace\07\rt-thread\include/rtthread.h \
+ D:\RT-ThreadStudio\workspace\07/rtconfig.h \
+ D:\RT-ThreadStudio\workspace\07\rt-thread\include/rtdebug.h \
+ D:\RT-ThreadStudio\workspace\07\rt-thread\include/rtdef.h \
+ D:\RT-ThreadStudio\workspace\07\rt-thread\include/rtservice.h \
+ D:\RT-ThreadStudio\workspace\07\rt-thread\include/rtm.h \
+ D:\RT-ThreadStudio\workspace\07\rt-thread\components\finsh/finsh.h \
+ D:\RT-ThreadStudio\workspace\07\rt-thread\components\drivers\include/drivers/spi.h
+
+D:\RT-ThreadStudio\workspace\07\rtconfig_preinc.h:
+
+D:\RT-ThreadStudio\workspace\07\rt-thread\include/rtthread.h:
+
+D:\RT-ThreadStudio\workspace\07/rtconfig.h:
+
+D:\RT-ThreadStudio\workspace\07\rt-thread\include/rtdebug.h:
+
+D:\RT-ThreadStudio\workspace\07\rt-thread\include/rtdef.h:
+
+D:\RT-ThreadStudio\workspace\07\rt-thread\include/rtservice.h:
+
+D:\RT-ThreadStudio\workspace\07\rt-thread\include/rtm.h:
+
+D:\RT-ThreadStudio\workspace\07\rt-thread\components\finsh/finsh.h:
+
+D:\RT-ThreadStudio\workspace\07\rt-thread\components\drivers\include/drivers/spi.h:
