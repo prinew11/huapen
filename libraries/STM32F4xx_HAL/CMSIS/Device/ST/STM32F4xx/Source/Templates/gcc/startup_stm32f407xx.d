aeaeda3e5a673be193575e7f005429a1572751fd
prinew11 <140534584+prinew11@users.noreply.github.com>
2023-07-26 01:02:19 +0800

    Delete libraries directory

--- libraries/STM32F4xx_HAL/CMSIS/Device/ST/STM32F4xx/Source/Templates/gcc/startup_stm32f407xx.d
+++
@@ -1,2 +0,0 @@
-libraries/STM32F4xx_HAL/CMSIS/Device/ST/STM32F4xx/Source/Templates/gcc/startup_stm32f407xx.o: \
- ../libraries/STM32F4xx_HAL/CMSIS/Device/ST/STM32F4xx/Source/Templates/gcc/startup_stm32f407xx.s
